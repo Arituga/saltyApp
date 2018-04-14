a3c35160bc0118688345ca3e02ea882274048442
Jesse Tinell <jessetinell@users.noreply.github.com>
2018-04-14 18:26:13 +0200

    clean

--- SaltyNutCodersApp/Controllers/HomeController.cs
+++ SaltyNutCodersApp/Controllers/HomeController.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using SaltyNutCodersApp.Models;
 using Microsoft.AspNetCore.Http;
 
 namespace SaltyNutCodersApp.Controllers
@@ -13,7 +8,6 @@ namespace SaltyNutCodersApp.Controllers
     {
         public IActionResult Index()
         {
-            
             var cookieOptions = new CookieOptions()
             {
                 Expires = DateTime.Now.AddDays(365)
@@ -21,25 +15,6 @@ namespace SaltyNutCodersApp.Controllers
             var guid = Guid.NewGuid();
             HttpContext.Response.Cookies.Append("userId", guid.ToString(), cookieOptions);
             return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
