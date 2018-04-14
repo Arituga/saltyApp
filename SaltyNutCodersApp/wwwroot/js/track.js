c8da4df01435ca6fa912e98b8ac6a6c9e73c9062
DESKTOP-BAEME14\gonma <gon.mal88@gmail.com>
2018-04-14 22:02:58 +0200

    cwoop

---
+++ SaltyNutCodersApp/wwwroot/js/track.js
@@ -0,0 +1,39 @@
+﻿(function () {
+    var db = firebase.database();
+
+    function readCookie(name) {
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+        }
+        return null;
+    }
+    var trackLocation = function () {
+        if (navigator.geolocation) {
+            var id = readCookie("userId");
+            var type = $('input[name="type"]:checked').val();
+            navigator.geolocation.getCurrentPosition(function (loc) {
+                console.log(loc);
+                $('.info').text('lat: ' + loc.coords.latitude + 'lng: ' + loc.coords.longitude);
+                firebase.database().ref('locations/' + id).set({
+                    lat: loc.coords.latitude, lng: loc.coords.longitude, type: type
+                });
+            }, function (err) { }, {
+                    enableHighAccuracy: true
+                });
+
+        }
+    }
+
+
+    $('.submit-location').on('click', function (e) {
+
+        e.preventDefault();
+        setInterval(trackLocation, 1000);
+        //startTrackingLocation();
+
+    });
+})();
